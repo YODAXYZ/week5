d9b8264258885f813636ca0fb4ac0073342f23f4
Александр <kas1yoda@gmail.com>
2020-12-19 18:18:59 +0300

    Library V1.0

---
+++ my_site/p_library/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import AuthorEdit, AuthorList, author_create_many
+
+app_name = 'p_library'
+urlpatterns = [
+    path('author/create/', AuthorEdit.as_view(), name='author_create'),
+    path('authors/', AuthorList.as_view(), name='author_list'),
+    path('author/create_many', author_create_many, name='author_create_many'),
+]
